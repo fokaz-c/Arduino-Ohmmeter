9cea9d24acd66febb2c1d866a92b5230046d23b5
Pritam-Shome <127998270+Pritam-Shome@users.noreply.github.com>
2024-10-19 00:40:06 +0530

    create a model for finaloutput and used hardcoded value for testing

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
+import { FinalOutput } from "../models/FinalOutput";
 
 
 export function getUsers(request: Request, response: Response){
-  response.send([]);
+  const x:FinalOutput = new FinalOutput();
+  
+  response.send(x);
 }
 
 export function getUserById(request: Request, response: Response) {
---
+++ src/models/FinalOutput.ts
@@ -0,0 +1,18 @@
+export class FinalOutput {
+    ID: number;
+    DateTime: Date;
+    HardwareOutputId: number;
+    
+    constructor() {
+        this.ID = 0;
+        this.DateTime= new Date(2018, 0O5, 0O5, 17, 23, 42, 11);  
+        this.HardwareOutputId=0;
+      }
+}
+
+// const x:FinalOutput = new FinalOutput();
+// console.log(x);
+
+
+
+
