cefde8fc9f4222bd0b65e1655c275642e551a5ae
-foxT <fokazmchakma4427@gmail.com>
2024-10-18 15:01:53 +0530

    added config for ts and initial project strucutre set

---
+++ src/handlers/users.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+
+export function getUsers(request: Request, response: Response){
+  response.send([]);
+}
+
+export function getUserById(request: Request, response: Response) {
+  response.send([{}]); 
+}
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+import usersRouter from './routes/users';
+
+const app = express();
+const PORT = 3000;
+
+app.use('/api/users', usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`Running on Port ${PORT}`);
+});
